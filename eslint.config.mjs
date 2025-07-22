f709e10dc4a859ff070d9f3305ca6706de7791f3
olliekeane123 <ollie.keane1@gmail.com>
2025-07-22 10:17:24 +0100

    go back to old prisma-client-js and configure eslint to ignore unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "src/generated/**/*",
+      "**/generated/**/*",
+      ".next/**",
+      "node_modules/**",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
 ];
 
